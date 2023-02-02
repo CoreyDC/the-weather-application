fc4d7b07ec911360e73c93929fdcd4de406139c7
Corey Calbreath <corey_calbreath@yahoo.com>
2023-02-01 19:35:17 -0600

    added function for api and bootstrap

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,23 +2,45 @@
 // GLOBAL VARIABLES
 var apiKey = 'b1e116f4023797b243caec34eaa26f25';
 const searchBtn = $('#searchBtn');
+let degreesSym = '\u00B0';
+
+// TODAYS WEATHER VARIABLES
+const currentContain = $('#todays-weather');
+const currentDay = $('#name-date');
+const currentTemp = $('#today-temp');
+const currentWind = $('#today-wind');
+const currentHumid = $('#today-hum');
+let date = dayjs().format('MMM D, YYYY');
+
+// FIVE DAY VARIABLES
 
 
-// get city from input city search
-searchBtn.on("click", function () {
+// CLICK EVENT TO SEARCH CITY AND FETCH API
+searchBtn.on("click", getApi);
+
+// FUNCTION RAN WHEN CLICKED
+// FUNCTION TO GET USER CITY INPUT THEN ADD IT TO THE API URL
+// FUNCTION THEN FETCHES URL AND RETURNS DATA (TEMPERATURE, WIND AND HUMIDITY) FOR CURRENT DATE TO THE SCREEN
+function getApi() {
     const citySearch = $('#search-city').val();
-    console.log(citySearch);
-});
 
-
-// create function to fetch weather dashboard api
-/* function getApi() {
-    let requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&appid=' + apiKey;
+    let requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&appid=' + apiKey + '&units=imperial';
     fetch(requestURL)
-        .then(function (response) {
-            return response.json();
+        .then(res => {
+            return res.json();
         })
-        .then(function (data) {
+        .then(data => {
+            // console logging data from url
             console.log(data);
-        })    
-} */
+            // getting TODAYS WEATHER data and rendering it to page
+            currentDay.text(`${data.name}: ${date}`)
+            currentTemp.text(`Temperature: ${data.main.temp} ${degreesSym}F`);
+            currentWind.text(`Wind: ${data.wind.speed} mph`);
+            currentHumid.text(`Humidity: ${data.main.humidity}%`);
+        });
+} 
+
+// FUNCTION FOR FIVE DAY FORECAST
+function fiveDay() {
+
+}
